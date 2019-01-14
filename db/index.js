ade8b2c71d220e84e740eaa791eb129040dc66cc
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2019-01-14 17:26:37 -0500

    Rename db uri env var

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize(process.env.DB_URI, require('./config.json'));
+const db = new Sequelize(process.env.PSQL_URI, require('./config.json'));
 const path = require('path');
 
 const users = db.import(path.join(__dirname, 'users.js'));
