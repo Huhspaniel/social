5880ce48f4ecc5a32ffcfbed68db1f08bef56d78
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2019-01-17 15:50:16 -0500

    Implement ng router

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PostsPageComponent } from './posts-page/posts-page.component';
+import { UsersPageComponent } from './users-page/users-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const appRoutes = [{
+  path: 'posts',
+  component: PostsPageComponent
+}, {
+  path: 'users',
+  component: UsersPageComponent
+}, {
+  path: '',
+  component: HomePageComponent
+}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsPageComponent,
+    UsersPageComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
