e206a0d014a8962039038954ab14d495d680e50c
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2019-01-18 18:21:41 -0500

    Add login route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,25 @@
 const db = require('../db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 module.exports = function (app) {
+    app.post('/api/login', async (req, res) => {
+        try {
+            const user = await db.users.find({ username: req.body.username });
+            const isValid = await bcrypt.compare(req.body.password, user.password);
+
+            if (isValid) {
+                const token = jwt.sign({
+                    iss: 'huhspaniel.com',
+                    sub: user.id,
+                    exp: Math.round((Date.now() + 8.64e+7) / 1000)
+                }, process.env.JWT_KEY);
+                res.json({ token });
+            }
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    })
     app.route('/api/users')
         .get(async (req, res) => {
             try {
