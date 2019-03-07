d912649a1a409b4f4568ac2e72f3929eb7e0f46e
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2019-03-07 00:15:44 -0500

    Add HOST env variable

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || 'localhost';
 const db = require('./db');
 const app = require('./app.js');
 
 db.sync().then(function(val) {
-    app.listen(PORT, function startServer() {
+    app.listen(PORT, HOST, function startServer() {
         console.log(`App listening on port ${PORT}`);
     });
 })
