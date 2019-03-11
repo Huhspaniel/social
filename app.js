f817b3fc0bb2f1c078d6e5aec1b7f5336ad6ac7c
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2019-03-11 14:59:37 -0400

    Fix static file server directory

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const app = express();
 const PROD = process.env.NODE_ENV === 'production';
 
 if (PROD) {
-    app.use(express.static(path.join(__dirname, 'client/dist')));
+    app.use(express.static(path.join(__dirname, 'client/dist/social')));
 }
 app.use(express.json());
 require('./routes/api-routes')(app);
 if (PROD) {
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+        res.sendFile(path.join(__dirname, 'client/dist/social/index.html'));
     });
 }
 module.exports = app;
