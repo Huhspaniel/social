ad4dc96e1f1be11db685ba91c83121ac9a69addb
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2019-03-12 18:47:41 -0400

    Do not allow null on votes model attributes

--- app/db/votes.js
+++ app/db/votes.js
@@ -2,18 +2,21 @@ module.exports = function (sequelize, types) {
     const votes = sequelize.define('votes', {
         user_id: {
             type: 'integer',
-            unique: 'vote'
+            unique: 'vote',
+            allowNull: false
         },
         post_id: {
             type: 'integer',
-            unique: 'vote'
+            unique: 'vote',
+            allowNull: false
         },
         val: {
             type: 'integer',
             validate: {
                 min: -1,
                 max: 1
-            }
+            },
+            allowNull: false
         }
     })
     
