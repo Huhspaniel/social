f6dd3c4fcc778984cef44272d87c27f5b8c9dc7a
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2019-03-13 05:12:51 -0400

    Add signup form Add functions for toggling between signup and login

--- client/src/app/login-modal/login-modal.component.ts
+++ client/src/app/login-modal/login-modal.component.ts
@@ -8,6 +8,14 @@ import { Component, OnInit, HostBinding, Input } from '@angular/core';
 export class LoginModalComponent implements OnInit {
   @HostBinding('class.hidden') @Input() hidden: boolean;
   @Input() selectedForm: string;
+  apiPath = () => `/api/${this.selectedForm === 'signup' ? 'users' : 'login'}`
+  toggleModal() {
+    if (this.selectedForm === 'login') {
+      this.selectedForm = 'signup';
+    } else {
+      this.selectedForm = 'login';
+    }
+  }
 
   constructor() { }
 
