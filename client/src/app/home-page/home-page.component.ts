557b050f3d37efeab5b2f86211e28c92d8ad561e
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2019-03-14 16:17:17 -0400

    Add post score and voting icons

--- client/src/app/home-page/home-page.component.ts
+++ client/src/app/home-page/home-page.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 
+const parsePosts = posts => posts.map(post => {
+  const score = post.votes.reduce((sum, { val }) => sum + val, 0);
+  post.score = score;
+  return post;
+})
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
+
 export class HomePageComponent implements OnInit {
 
   feed: Array<any>;
@@ -13,8 +20,9 @@ export class HomePageComponent implements OnInit {
 
   async refreshFeed() {
     try {
-      this.feed = await this.api.get('posts');
-      console.log(this.feed);
+      const posts = await this.api.get('posts');
+      console.log(posts);
+      this.feed = parsePosts(posts);
     } catch (err) {
       console.error(err);
     }
